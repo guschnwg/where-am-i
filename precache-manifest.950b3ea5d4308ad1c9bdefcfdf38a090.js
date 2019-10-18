fff60dcce36fad198a5fb913309140e83e9a2d8d
Gustavo Zanardini <guschoneweg@hotmail.com>
2019-10-18 15:17:57 -0300

    Updates

---
+++ precache-manifest.950b3ea5d4308ad1c9bdefcfdf38a090.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63615cce1b2033a1986a3f503f792a6e",
+    "url": "/where-am-i/index.html"
+  },
+  {
+    "revision": "011cb0e681937a0bd849",
+    "url": "/where-am-i/static/js/2.887e6980.chunk.js"
+  },
+  {
+    "revision": "f2c119f6262bf05441ff",
+    "url": "/where-am-i/static/js/main.9ca93935.chunk.js"
+  },
+  {
+    "revision": "5327c9e6a36a2f02e25c",
+    "url": "/where-am-i/static/js/runtime-main.fafd70a6.js"
+  }
+]);
