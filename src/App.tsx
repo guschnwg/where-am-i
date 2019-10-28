c310bfa5990d6c0a565a6cd60c22f84b452b0649
Gustavo Zanardini <guschoneweg@hotmail.com>
2019-10-28 12:14:45 -0300

    Added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
 import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  // @ts-ignore
+} from 'react-router-dom'
 import GameScreen from './screens/Game'
 import { MapsContextProvider } from './context/Maps'
 
 const App: React.FC = () => {
   return (
     <MapsContextProvider>
-      <GameScreen />
+      <Router>
+        <Switch>
+          <Route path="/guessing">
+            <GameScreen />
+          </Route>
+          <Route path="/">
+            <Link to="/guessing">Guessing game</Link>
+          </Route>
+        </Switch>
+      </Router>
     </MapsContextProvider>
   )
 }
